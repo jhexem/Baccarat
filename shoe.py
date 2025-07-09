3b1151d84d4094bf1957b05a42b71ce914474a7b
Justin Hexem <justinbhexem@icloud.com>
2025-07-08 20:44:45 -0700

    first commit

---
+++ shoe.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class Shoe:
+   cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+   values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 0,
+               'J': 0, 'Q': 0, 'K': 0, 'A': 1}
+   
+   def __init__(self, numDecks):
+      self.numDecks = numDecks
+      self.deck = self.createDeck()
+      self.shuffle()
+      
+   def createDeck(self):
+      deck = []
+      for _ in range(self.numDecks * 4):
+         for card in self.cards:
+            deck.append((card, self.values[card]))
+      return deck
+      
+   def dealCard(self):
+      if len(self.deck) == 0:
+         raise ValueError("No cards left in the shoe.")
+      return self.deck.pop()
+   
+   def shuffle(self):
+      self.deck = self.createDeck()
+      np.random.shuffle(self.deck)
+      
+   def getValue(self, card):
+      return self.values[card]
+   
+   def remainingCards(self):
+      return len(self.deck) * 100 / (self.numDecks * 52)
